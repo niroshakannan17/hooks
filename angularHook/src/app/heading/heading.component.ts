69999539f6835f3ed6e670e55a86f6ffa5c91edc
nirosha <niroshakannan17@gmail.com>
2024-04-09 16:25:19 +0530

    ngOnInit hooks example worked

--- angularHook/src/app/heading/heading.component.ts
+++ angularHook/src/app/heading/heading.component.ts
@@ -2,22 +2,28 @@ import { Component, SimpleChange, SimpleChanges } from '@angular/core';
 import { Input } from '@angular/core';
 
 import { OnChanges } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-heading',
   templateUrl: './heading.component.html',
   styleUrl: './heading.component.css'
 })
-export class HeadingComponent implements OnChanges{
+export class HeadingComponent implements OnChanges, OnInit{
     
     @Input('headingTitle')  actuval_text = '';
 
     ngOnChanges(changes: SimpleChanges): void {
       
       if(!changes['actuval_text'].firstChange){
-        console.log(changes['actuval_text'].firstChange);
+        //console.log(changes['actuval_text'].currentValue);
         console.log('ngOnchange hook is calles');
       }
-      
+      console.log('ngOnchange hook is calles');
+    }
+
+    ngOnInit(): void {
+      console.log('ngOnInit hook is calles');
+      this.actuval_text ="first comment";
     }
 }
